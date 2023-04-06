184c7e53e8a78a589cb7b814076efdc456c07881
MrShtekman <37696604+MrShtekman@users.noreply.github.com>
2023-04-06 15:16:50 +0200

    2

---
+++ Assets/GraphicsSetting.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphicsSetting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.XR.XRSettings.eyeTextureResolutionScale = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Script/ShowTeleportRay.cs
+++ Assets/Script/ShowTeleportRay.cs
@@ -20,6 +20,6 @@ public class ShowTeleportRay : MonoBehaviour
     {
         reticle.SetActive(false);
         teleportRay.enabled = false;
-
+        
     }
 }
